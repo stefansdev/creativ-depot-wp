d07a42c13bff7ac443027b18e46bc5e8a08689f4
Stefan Stankovic <s@stefans.dev>
2021-02-18 15:32:22 +0100

    gulp update tailwind

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,16 +5,12 @@ import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
-import autoprefixer from 'autoprefixer';
 import image from 'gulp-image';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
 import plumber from 'gulp-plumber';
-import notify from "gulp-notify";
 
 import del from 'del';
-import webpack from 'webpack-stream';
-import named from 'vinyl-named';
 import browserSync from "browser-sync";
 import zip from "gulp-zip";
 import svgstore from 'gulp-svgstore';
@@ -26,93 +22,94 @@ import config from "./gulpconfig.json";
 const PRODUCTION = yargs.argv.prod;
 const server = browserSync.create();
 export const serve = done => {
-    server.init({
-        proxy: config.proxy
-    });
-    done();
+	server.init({
+		proxy: config.proxy
+	});
+	done();
 };
 export const reload = done => {
-    server.reload();
-    done();
+	server.reload();
+	done();
 };
 export const clean = () => del(config.cleanFiles);
 
 export const styles = () => {
-	const tailwindcss = require('tailwindcss'); 
-	
-    return src(config.sourcePaths.scss)
-	.pipe(gulpif(!PRODUCTION, sourcemaps.init()))
-	.pipe(sass()).on('error', sass.logError)
-	.pipe(postcss([
-		require('tailwindcss'),
-		require('autoprefixer')
-	]))
-	.pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
-    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-	.pipe(dest(config.deployPaths.scss))
-	.pipe(server.stream());
-	
+	const tailwindcss = require('tailwindcss');
+
+	return src(config.sourcePaths.scss)
+		.pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+		.pipe(sass()).on('error', sass.logError)
+		.pipe(gulpif(PRODUCTION, postcss([
+			require('tailwindcss'),
+			require('autoprefixer')
+		])))
+		.pipe(gulpif(PRODUCTION, cleanCss({ compatibility: 'ie8' })))
+		.pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+		.pipe(dest(config.deployPaths.scss))
+		.pipe(server.stream());
+
 }
 export const images = () => {
-    return src(config.sourcePaths.images)
-	.pipe(image())
-    .pipe(dest(config.deployPaths.images));
+	return src(config.sourcePaths.images)
+		.pipe(image())
+		.pipe(dest(config.deployPaths.images));
 }
 
 
 
 export const svg = () => {
-    return src(config.sourcePaths.svg)
-    .pipe(svgstore({
-        inlineSvg: true
-    }))
-    .pipe(cheerio({
-        run: function ($) {
-            $('svg').attr('style', 'display:none');
-        },
-        parserOptions: {
-            xmlMode: true
-        }
-    }))
-    .pipe(dest(config.deployPaths.svg));
+	return src(config.sourcePaths.svg)
+		.pipe(svgstore({
+			inlineSvg: true
+		}))
+		.pipe(cheerio({
+			run: function ($) {
+				$('svg').attr('style', 'display:none');
+			},
+			parserOptions: {
+				xmlMode: true
+			}
+		}))
+		.pipe(dest(config.deployPaths.svg));
 }
 
 export const scripts = () => {
-    return src(config.sourcePaths.js)
-        .pipe(uglify())
-        .pipe(plumber())
-        .pipe(concat(config.deployFiles.js))
-        .pipe(dest(config.deployPaths.js));
+	return src(config.sourcePaths.js)
+		.pipe(uglify())
+		.pipe(plumber())
+		.pipe(concat(config.deployFiles.js))
+		.pipe(dest(config.deployPaths.js));
 }
 export const compress = () => {
-    return src([
-        "**/*",
-        "!node_modules{,/**}",
-        "!bundled{,/**}",
-        "!src{,/**}",
-        "!.babelrc",
-        "!.gitignore",
-        "!gulpfile.babel.js",
-        "!package.json",
-        "!package-lock.json",
-    ])
-    .pipe(
-        gulpif(
-            file => file.relative.split(".").pop() !== "zip",
-            replace("_themename", info.name)
-        )
-    )
-    .pipe(zip(`${info.name}.zip`))
-    .pipe(dest('bundled'));
+	return src([
+		"**/*",
+		"!node_modules{,/**}",
+		"!bundled{,/**}",
+		"!src{,/**}",
+		"!.babelrc",
+		"!.gitignore",
+		"!gulpfile.babel.js",
+		"!package.json",
+		"!package-lock.json",
+	])
+		.pipe(
+			gulpif(
+				file => file.relative.split(".").pop() !== "zip",
+				replace("_themename", info.name)
+			)
+		)
+		.pipe(zip(`${info.name}.zip`))
+		.pipe(dest('bundled'));
 };
 
 export const watchForChanges = () => {
-    watch(config.sourcePaths.scss, styles);
-    watch(config.sourcePaths.images, series(images, reload));
-    watch(config.sourcePaths.js, series(scripts, reload));
-    watch(config.sourcePaths.svg, svg);
-    watch(config.sourcePaths.php, reload);
+	watch([config.sourcePaths.tailwind, config.sourcePaths.scss], styles);
+	watch(config.sourcePaths.images, series(images, reload));
+	watch(config.sourcePaths.js, series(scripts, reload));
+	watch(config.sourcePaths.svg, svg);
+	watch(config.sourcePaths.php, series(reload));
+	// watch(config.sourcePaths.php, series(styles, reload));
 }
 export const dev = series(clean, parallel(styles, images, svg, scripts), serve, watchForChanges);
-export const build = series(clean, parallel(styles, images, svg, scripts), compress);
+export const build = series(clean, parallel(styles, images, svg, scripts));
 export default dev;
--- wp-content/themes/anomys/header.php
+++ wp-content/themes/anomys/header.php
@@ -5,6 +5,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
