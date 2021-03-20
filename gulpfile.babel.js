68caa6d5b13bd1683ef4bd962b1cb85cbec1e4dc
Stefan Stankovic <s@stefans.dev>
2021-03-20 21:07:51 +0100

    updates on tailwind... issue with not compiling tailwind on scss save

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import postcss from 'gulp-postcss';
+import postcssImport from 'postcss-import';
 import sourcemaps from 'gulp-sourcemaps';
 import image from 'gulp-image';
 import concat from 'gulp-concat';
@@ -12,11 +13,12 @@ import plumber from 'gulp-plumber';
 
 import del from 'del';
 import browserSync from "browser-sync";
-import zip from "gulp-zip";
 import svgstore from 'gulp-svgstore';
 import cheerio from 'gulp-cheerio';
-import info from "./package.json";
 import replace from "gulp-replace";
+import tailwindcss from "@tailwindcss/jit";
+import autoprefixer from 'autoprefixer';
+import postScss from 'postcss-scss'
 import config from "./gulpconfig.json";
 
 const PRODUCTION = yargs.argv.prod;
@@ -32,23 +34,25 @@ export const reload = done => {
 	done();
 };
 export const clean = () => del(config.cleanFiles);
+export const cleanDevCss = () => del(config.cleanCSS);
 
 export const styles = () => {
-	const tailwindcss = require('tailwindcss');
-
+	var plugins = [
+		tailwindcss(config.tailwindjs),
+		autoprefixer(),
+	];
 	return src(config.sourcePaths.scss)
 		.pipe(gulpif(!PRODUCTION, sourcemaps.init()))
 		.pipe(sass()).on('error', sass.logError)
-		.pipe(postcss([
-			require('@tailwindcss/jit'),
-			require('autoprefixer')
-		]))
+		.pipe(postcss(plugins, { parser: postScss }))
 		.pipe(gulpif(PRODUCTION, cleanCss({ compatibility: 'ie8' })))
+		.pipe((concat({ path: "app.css" })))
 		.pipe(gulpif(!PRODUCTION, sourcemaps.write()))
 		.pipe(dest(config.deployPaths.scss))
 		.pipe(server.stream());
-
 }
+
+
 export const images = () => {
 	return src(config.sourcePaths.images)
 		.pipe(image())
@@ -80,35 +84,14 @@ export const scripts = () => {
 		.pipe(concat(config.deployFiles.js))
 		.pipe(dest(config.deployPaths.js));
 }
-export const compress = () => {
-	return src([
-		"**/*",
-		"!node_modules{,/**}",
-		"!bundled{,/**}",
-		"!src{,/**}",
-		"!.babelrc",
-		"!.gitignore",
-		"!gulpfile.babel.js",
-		"!package.json",
-		"!package-lock.json",
-	])
-		.pipe(
-			gulpif(
-				file => file.relative.split(".").pop() !== "zip",
-				replace("_themename", info.name)
-			)
-		)
-		.pipe(zip(`${info.name}.zip`))
-		.pipe(dest('bundled'));
-};
 
 export const watchForChanges = () => {
-	watch([config.sourcePaths.tailwind, config.sourcePaths.scss], styles);
+	watch(config.sourcePaths.scss, series(styles));
+	watch(config.sourcePaths.tailwind, series(styles));
 	watch(config.sourcePaths.images, series(images, reload));
 	watch(config.sourcePaths.js, series(scripts, reload));
 	watch(config.sourcePaths.svg, svg);
-	watch(config.sourcePaths.php, series(reload));
-	// watch(config.sourcePaths.php, series(styles, reload));
+	watch(config.sourcePaths.php, series(styles, reload));
 }
 export const dev = series(clean, parallel(styles, images, svg, scripts), serve, watchForChanges);
 export const build = series(clean, parallel(styles, images, svg, scripts));
--- wp-content/themes/anomys/header.php
+++ wp-content/themes/anomys/header.php
@@ -5,7 +5,6 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
@@ -13,7 +12,7 @@
 <body <?php body_class(); ?>>
 
 
-	<header class="bg-red-500 flex justify-center ">
+	<header class="bg-blue-500 flex justify-center ">
 		<nav>
 			<?php
 			$args_menu = array(
